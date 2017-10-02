08ecbac5ae2faeb0519ebecff3081aa30cd2729c
fals0827 <fals0827@gmail.com>
2017-10-02 15:53:34 +0800

    HW done

---
+++ src/HW13.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HW13 {
+        public static void main(String[] args) {
+            Scanner scn = new Scanner(System.in);
+            char v1 = scn.next().charAt(0);
+            System.out.println((int)v1);
+
+    }
+}
